fdc549023599f837fd74aad7bea3e23feddd08bf
Tanveer <i.kazitanveer@gmail.com>
2023-08-17 12:50:54 +0600

    First Commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//console.log("First Line");
+//console.log("Second Line");
+window.alert("I am Kazi Tanveer Islam From Java Script Course");
+var a = prompt("Enter your number: ");
+
+var b= 9/5*a + 32;
+
+alert(b);
+
+
+//document.write("Alhamdulillah, my JS file has been connected.");
+
+
